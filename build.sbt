a3e497024bf37fd008e01ada49eed5f5058f38f8
Demian Neidetcher <demian0311@gmail.com>
2013-04-18 13:44:54 -0500

    add junit interface, so weird that you need that

--- build.sbt
+++ build.sbt
@@ -8,5 +8,7 @@ libraryDependencies += "com.twitter" %% "finagle-http" % "6.2.0"
 
 libraryDependencies += "junit" % "junit" % "4.11" % "test"
 
+libraryDependencies += "com.novocode" % "junit-interface" % "0.8" % "test->default"
+
 addCommandAlias("run-scipr", "run src/test/resources/scipr-config.xml")
 
--- src/test/scala/scipr/ConfigFinderTest.scala
+++ src/test/scala/scipr/ConfigFinderTest.scala
@@ -3,15 +3,10 @@ package scipr
 import org.junit.Test
 
 class ConfigFinderTest {
-    
-  
     @Test def test(){
         println("hello world")
         val unit = new ConfigFinder()
         val result = unit.xmlFromFilename("src/test/resources/scipr-config.xml")
         println("result: " + result)
-        
     }
-  
-
 }
